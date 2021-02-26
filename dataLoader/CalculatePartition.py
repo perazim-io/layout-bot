6089db81f978d72e4e5e3303218fd2ff78abfe51
adiak <adiakash98@gmail.com>
2021-02-26 21:40:55 +0530

    m1

--- dataLoader/CalculatePartition.py
+++ dataLoader/CalculatePartition.py
@@ -6,8 +6,13 @@ def getPartitionPercentage(canvasDataArray):
     categoryA = 0
     categoryB = 0
     categoryC = 0
+    maxWidth = 0
+
     for document in canvasDataArray:
         category = document.info.category
+        width = document.info.width
+        if width > maxWidth:
+            maxWidth = width
         if category == 'A':
             categoryA = categoryA+1
         elif category == 'B':
@@ -15,14 +20,21 @@ def getPartitionPercentage(canvasDataArray):
         elif category == 'C':
             categoryC = categoryC+1
 
-    sum= categoryA + categoryB + categoryC
+    sum = categoryA + categoryB + categoryC
 
-    percentageA = (categoryA/sum) * 100
-    percentageB = (categoryB/sum) * 100
-    percentageC = (categoryC/sum) * 100
+    pixelA = (categoryA/sum) * 1080
+    pixelB = (categoryB/sum) * 1080
+    pixelC = (categoryC/sum) * 1080
 
-    percentage['A'] = percentageA
-    percentage['B'] = percentageB
-    percentage['C'] = percentageC
+    if maxWidth > pixelA:
+        pixelA = maxWidth + 20
+    if maxWidth > pixelB:
+        pixelB = maxWidth + 20
+    if maxWidth > pixelC:
+        pixelC = maxWidth + 20
+
+    percentage['A'] = pixelA
+    percentage['B'] = pixelB
+    percentage['C'] = pixelC
 
     return percentage
--- main.py
+++ main.py
@@ -7,3 +7,6 @@ canvasDataArray = getData()
 partitionPercentage = getPartitionPercentage(canvasDataArray)
 
 print(partitionPercentage)
+
+for i in canvasDataArray:
+    print(i.info.category)
