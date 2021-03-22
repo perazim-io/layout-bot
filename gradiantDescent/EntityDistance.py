a2e07ad41f3924cb24e19d49f218c6e67e6eca92
adiak <adiakash98@gmail.com>
2021-03-22 16:58:58 +0530

    m1

--- gradiantDescent/EntityDistance.py
+++ gradiantDescent/EntityDistance.py
@@ -9,24 +9,25 @@ def getRelationDistance(entityData, x=650, y=740):
     p = (x1, y1)
     xFixed = getDistanceDictionaryX(p[0], x, y)
     yFixed = getDistanceDictionaryY(p[1], x, y)
+    print(xFixed)
     return 0
 
 
 def getDistanceDictionaryX(coord, x, y):
     point1 = (x, y)
-    xFixed = {}
+    xFixed = {point1: []}
     for i in range(1079):
         point2 = (coord, i)
         d = distance.euclidean(point1, point2)
-        xFixed[point1].append([coord, d])
+        xFixed[point1].append([point2, d])
     return xFixed
 
 
 def getDistanceDictionaryY(coord, x, y):
     point1 = (x, y)
-    yFixed = {}
+    yFixed = {point1: []}
     for i in range(1079):
         point2 = (i, coord)
-        d = distance.eucledian(point1, point2)
-        yFixed[point1].append([coord,d])
+        d = distance.euclidean(point1, point2)
+        yFixed[point1].append([point2, d])
     return yFixed
